50e69ae3ef851660e01ef32573523713693f7366
Dinghao Zeng <dinghao.zeng@gmail.com>
2023-03-01 13:47:05 +0800

    swifty animtool

---
+++ xcode/SwiftyAnimTool.h
@@ -0,0 +1,20 @@
+//
+//  SwiftyAnimTool.h
+//  SwiftyAnimTool
+//
+//  Created by Dinghao Zeng on 2023/3/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyAnimTool.
+FOUNDATION_EXPORT double SwiftyAnimToolVersionNumber;
+
+//! Project version string for SwiftyAnimTool.
+FOUNDATION_EXPORT const unsigned char SwiftyAnimToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyAnimTool/PublicHeader.h>
+
+
+#include "dropframes.h"
+#include "animate.h"
