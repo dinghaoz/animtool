41df32a3725ca1a417f0a5de8bb2a3d3ca1f03bf
Dinghao Zeng <dinghao.zeng@gmail.com>
2023-03-01 11:20:05 +0800

    animtool ios

---
+++ animtool/animtool/animtool.h
@@ -0,0 +1,18 @@
+//
+//  animtool.h
+//  animtool
+//
+//  Created by Dinghao Zeng on 2023/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for animtool.
+FOUNDATION_EXPORT double animtoolVersionNumber;
+
+//! Project version string for animtool.
+FOUNDATION_EXPORT const unsigned char animtoolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <animtool/PublicHeader.h>
+
+
