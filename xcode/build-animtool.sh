f4ba69097a70d1b49021e6d32d8a3925eaa6c499
Dinghao Zeng <dinghao.zeng@gmail.com>
2023-03-01 15:58:04 +0800

    script

---
+++ xcode/build-animtool.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#  cmake-build.sh
+#  SwiftyAnimTool
+#
+#  Created by Dinghao Zeng on 2023/3/1.
+#  
+
+echo ${TARGET_BUILD_DIR}
+echo "EFFECTIVE_PLATFORM_NAME $EFFECTIVE_PLATFORM_NAME."
+
+PLATFORM=OS64
+if [[ $EFFECTIVE_PLATFORM_NAME == "-iphonesimulator" ]]
+then
+  PLATFORM=SIMULATOR64
+  echo "The variable is greater than $PLATFORM."
+fi
+
+
+mkdir -p ${TARGET_BUILD_DIR}/cmake
+mkdir -p ${TARGET_BUILD_DIR}/cmake-products
+cmake -S ${PROJECT_DIR}/.. -B ${TARGET_BUILD_DIR}/cmake -GXcode -DCMAKE_TOOLCHAIN_FILE=${PROJECT_DIR}/../cmake/ios.toolchain.cmake -DPLATFORM=$PLATFORM -DENABLE_BITCODE=FALSE
+cmake --build ${TARGET_BUILD_DIR}/cmake --config ${CONFIGURATION}
+
+LIB_PATH_PARTS=("" /_deps/webp-build /_deps/jpeg-build)
+for i in "${LIB_PATH_PARTS[@]}"
+do
+    LIB_PATH=${TARGET_BUILD_DIR}/cmake$i/${CONFIGURATION}$EFFECTIVE_PLATFORM_NAME
+	echo "$LIB_PATH"
+    for FILE_PATH in "$LIB_PATH"/*
+    do
+        FILE_NAME="$(basename "$FILE_PATH")"
+        rm -f ${TARGET_BUILD_DIR}/cmake-products/$FILE_NAME
+        ln -s $FILE_PATH ${TARGET_BUILD_DIR}/cmake-products/$FILE_NAME
+    done
+done
