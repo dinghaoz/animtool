1326c2d1d5a079d563436eabd846a912d6504b4c
Dinghao Zeng <dinghao.zeng@gmail.com>
2023-07-26 18:35:12 +0800

    consolidate dup code

---
+++ app/output_flags.h
@@ -0,0 +1,16 @@
+//
+// Created by Dinghao Zeng on 2023/7/26.
+//
+
+#ifndef ANIMTOOL_OUTPUT_FLAGS_H
+#define ANIMTOOL_OUTPUT_FLAGS_H
+
+
+namespace cli {
+    struct Cmd;
+} // namespace cli
+
+void CmdAddOutputFlags(cli::Cmd* cmd);
+
+
+#endif //ANIMTOOL_OUTPUT_FLAGS_H
--- app/overlay_cmd.cpp
+++ app/overlay_cmd.cpp
@@ -5,6 +5,7 @@
 #include "overlay_cmd.h"
 
 #include "cli.h"
+#include "output_flags.h"
 #include "core/overlay.h"
 #include "core/logger.h"
 #include "core/check.h"
@@ -137,58 +138,5 @@ void CmdOverlayInit(cli::Cmd* cmd) {
             .default_value = { .int_value = 0 }
     });
 
-    cmd->AddFlag(cli::Flag{
-            .name = "minimize_size",
-            .desc = "If true, minimize the output size (slow). Implicitly disables key-frame insertion.",
-            .type = cli::FLAG_BOOL,
-            .required = 0,
-            .multiple = 0,
-            .default_value = { .bool_value = 0 }
-    });
-
-    cmd->AddFlag(cli::Flag{
-            .name = "verbose",
-            .aliases = {"v"},
-            .desc = "print detail log",
-            .type = cli::FLAG_BOOL,
-            .required = 0,
-            .multiple = 0,
-            .default_value = { .bool_value = 0 }
-    });
-
-    cmd->AddFlag(cli::Flag{
-            .name = "lossless",
-            .desc = "Lossless encoding.",
-            .type = cli::FLAG_BOOL,
-            .required = 0,
-            .multiple = 0,
-            .default_value = { .bool_value = 0 }
-    });
-
-    cmd->AddFlag(cli::Flag{
-            .name = "quality",
-            .desc = "between 0 and 100.",
-            .type = cli::FLAG_FLOAT,
-            .required = 0,
-            .multiple = 0,
-            .default_value = { .float_value = 75.0f }
-    });
-
-    cmd->AddFlag(cli::Flag{
-            .name = "method",
-            .desc = "quality/speed trade-off (0=fast, 6=slower-better).",
-            .type = cli::FLAG_INT,
-            .required = 0,
-            .multiple = 0,
-            .default_value = { .int_value = 0 }
-    });
-
-    cmd->AddFlag(cli::Flag{
-            .name = "pass",
-            .desc = "number of entropy-analysis passes (in [1..10]).",
-            .type = cli::FLAG_INT,
-            .required = 0,
-            .multiple = 0,
-            .default_value = { .int_value = 1 }
-    });
+    CmdAddOutputFlags(cmd);
 }
